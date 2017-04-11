9ce856659e1ff74e6268be1bb4eaf2c9f97e75c7
Anwesh43 <anweshthecool0@gmail.com>
2017-04-11 10:46:47 +0530

    adding onupdate listener

--- collapsiblelistview/src/main/java/com/anwesome/ui/collapsiblelistview/CollapsibleListView.java
+++ collapsiblelistview/src/main/java/com/anwesome/ui/collapsiblelistview/CollapsibleListView.java
@@ -15,6 +15,7 @@ import android.widget.LinearLayout;
  */
 public class CollapsibleListView extends ViewGroup {
     private  int w,h;
+    private int viewSize;
     public CollapsibleListView(Context context) {
         super(context);
         initDimensions(context);
@@ -27,29 +28,36 @@ public class CollapsibleListView extends ViewGroup {
             display.getRealSize(size);
             w = size.x;
             h = size.y;
+            viewSize = (9*w)/10;
         }
     }
     public void addCollapsibleItem(Bitmap bitmap) {
         CollapsibleItem collapsibleItem = new CollapsibleItem(getContext(),bitmap);
-        addView(collapsibleItem,new ViewGroup.LayoutParams(9*w/10,9*w/10));
+        collapsibleItem.setOnUpdateListener(new CollapsibleItem.OnUpdateListener() {
+            @Override
+            public void onUpdate() {
+                requestLayout();
+            }
+        });
+        addView(collapsibleItem,new ViewGroup.LayoutParams(viewSize,viewSize));
         requestLayout();
     }
     public void onLayout(boolean reloaded,int a,int b,int w2,int h1) {
-        float y = 0;
+        int y = h/40;
         for(int i=0;i<getChildCount();i++) {
             View child = getChildAt(i);
             CollapsibleItem collapsibleItem = (CollapsibleItem)child;
-            int viewH = collapsibleItem.getMeasuredHeight();
-            collapsibleItem.layout(w/20,(int)y,(19*w)/20,(int)(y+viewH+(4*viewH*collapsibleItem.getScale())));
-            y+=viewH/5+viewH*collapsibleItem.getScale()+h/20;
+            collapsibleItem.layout(w/20,y,(19*w)/20,(y+collapsibleItem.getMeasuredHeight()));
+            y+=(collapsibleItem.getMeasuredHeight()/5+(int)((4*collapsibleItem.getMeasuredHeight()/5)*collapsibleItem.getScale()))+h/40;
         }
     }
     public void onMeasure(int wspec,int hspec) {
-        int hNew = 0;
+        int hNew = h/40;
         for(int i=0;i<getChildCount();i++) {
             View child = getChildAt(i);
             measureChild(child,wspec,hspec);
-            hNew+=child.getMeasuredHeight();
+            CollapsibleItem collapsibleItem = (CollapsibleItem)child;
+            hNew+=(collapsibleItem.getMeasuredHeight()/5+(int)((4*collapsibleItem.getMeasuredHeight()/5)*collapsibleItem.getScale()))+h/40;
         }
         hNew = Math.max(h,hNew);
         setMeasuredDimension(w,hNew);
