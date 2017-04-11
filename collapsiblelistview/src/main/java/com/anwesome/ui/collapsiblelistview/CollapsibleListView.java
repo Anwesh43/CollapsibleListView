e25e60e4c3e3a7e3a96304cb86f4b7c162428713
Anwesh43 <anweshthecool0@gmail.com>
2017-04-11 05:40:17 +0530

    Adding CollapsibleListView

---
+++ collapsiblelistview/src/main/java/com/anwesome/ui/collapsiblelistview/CollapsibleListView.java
@@ -0,0 +1,19 @@
+package com.anwesome.ui.collapsiblelistview;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+/**
+ * Created by anweshmishra on 11/04/17.
+ */
+public class CollapsibleListView extends ViewGroup {
+    public CollapsibleListView(Context context) {
+        super(context);
+    }
+    public void onMeasure(int wspec,int hspec) {
+
+    }
+    public void onLayout(boolean reloaded,int a,int b,int w,int h) {
+
+    }
+}
