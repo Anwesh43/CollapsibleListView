04792cde5e839071da13e9302d3bf3757405a01d
Anwesh43 <anweshthecool0@gmail.com>
2017-04-11 08:04:37 +0530

    Adding CollapsibleListView

--- collapsiblelistview/src/main/java/com/anwesome/ui/collapsiblelistview/CollapsibleListView.java
+++ collapsiblelistview/src/main/java/com/anwesome/ui/collapsiblelistview/CollapsibleListView.java
@@ -1,19 +1,35 @@
 package com.anwesome.ui.collapsiblelistview;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.hardware.display.DisplayManager;
+import android.view.Display;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 /**
  * Created by anweshmishra on 11/04/17.
  */
-public class CollapsibleListView extends ViewGroup {
+public class CollapsibleListView extends LinearLayout {
+    private  int w,h;
     public CollapsibleListView(Context context) {
         super(context);
+        initDimensions(context);
     }
-    public void onMeasure(int wspec,int hspec) {
-
+    public void initDimensions(Context context) {
+        DisplayManager displayManager = (DisplayManager)context.getSystemService(Context.DISPLAY_SERVICE);
+        Display display = displayManager.getDisplay(0);
+        if(display!=null) {
+            Point size = new Point();
+            display.getRealSize(size);
+            w = size.x;
+            h = size.y;
+        }
     }
-    public void onLayout(boolean reloaded,int a,int b,int w,int h) {
-
+    public void addCollapsibleItem(Bitmap bitmap) {
+        CollapsibleItem collapsibleItem = new CollapsibleItem(getContext(),bitmap);
+        addView(collapsibleItem,new ViewGroup.LayoutParams(9*w/10,9*w/10));
     }
 }
