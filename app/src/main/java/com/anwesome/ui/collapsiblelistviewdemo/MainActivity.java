da6ab11ee8f02b0b60ac78cd14e540d296c1476e
Anwesh43 <anweshthecool0@gmail.com>
2017-04-11 10:49:20 +0530

    Using CollapsibleListView in app

--- app/src/main/java/com/anwesome/ui/collapsiblelistviewdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/collapsiblelistviewdemo/MainActivity.java
@@ -1,13 +1,26 @@
 package com.anwesome.ui.collapsiblelistviewdemo;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
+
+import com.anwesome.ui.collapsiblelistview.CollapsibleListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int drawables[] = {R.drawable.back1,R.drawable.back2,R.drawable.back3,R.drawable.back4};
+    private CollapsibleListView collapsibleListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ScrollView scrollView = new ScrollView(this);
+        collapsibleListView = new CollapsibleListView(this);
+        for(int i=0;i<drawables.length;i++) {
+            collapsibleListView.addCollapsibleItem(BitmapFactory.decodeResource(getResources(),drawables[i]));
+        }
+        scrollView.addView(collapsibleListView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        setContentView(scrollView);
     }
 }
